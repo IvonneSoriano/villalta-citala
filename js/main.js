78752242909fbc181c61ed8a16801f3ed7a32cb3
Ivonne Sanchez <ivonnesoriano25@gmail.com>
2024-07-01 21:07:05 -0600

    added text and colors

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-    $(".nube").click(function(){
-        console.log("LCI")
-      $(".blue").addClass("in");
-      $(".pink").addClass("in");
-    });
+    // $(".nube").click(function(){
+    //     console.log("LCI")
+    //   $(".blue").addClass("in");
+    //   $(".pink").addClass("in");
+    // });
+    setTimeout(()=>{
+      $(".animals").addClass("in");
+    },500)
   });
